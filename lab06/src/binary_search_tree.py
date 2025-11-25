736a456fa04bb70ec02bebfa625b6d3f009fde2d
y-ivanovv <yurastav126@mail.ru>
2025-11-26 00:57:03 +0300

    add bst

---
+++ lab06/src/binary_search_tree.py
@@ -0,0 +1,150 @@
+from typing import Optional
+
+
+class TreeNode:
+    """Узел бинарного дерева поиска"""
+
+    def __init__(self, value: int):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+
+    def __str__(self):
+        return f"Значение узла: {self.value}, Родитель узла: {self.parent}, Левый потомок: {self.left}, Правый потомок:{self.right}"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class BinaryTree:
+    """Бинарное дерево поиска"""
+
+    def __init__(self):
+        self.root = None
+        self._size = 0
+
+    def insert(self, value: int) -> TreeNode:
+        """
+        Вставка элемента в дерево
+        Сложность: в среднем O(log n), в худшем O(n)
+        """
+        new_node = TreeNode(value)
+
+        if self.root is None:
+            self.root = new_node
+            self._size += 1
+            return new_node
+
+        current = self.root
+        parent = None
+
+        while current is not None:
+            parent = current
+            if value < current.value:
+                current = current.left
+            elif value > current.value:
+                current = current.right
+            else:
+                return current
+
+        new_node.parent = parent
+        if value < parent.value:
+            parent.left = new_node
+        elif value > parent.value:
+            parent.right = new_node
+
+        self._size += 1
+        return new_node
+
+    def search(self, value: int) -> Optional[TreeNode]:
+        """
+        Поиск элемента в дереве
+        Сложность: в среднем O(log n), в худшем O(n)
+        """
+        return self._search_from_node(self.root, value)
+
+    def _search_from_node(self, node: TreeNode, value: int) -> Optional[TreeNode]:
+        """Рекурсивный вспомогательный метод для поиска"""
+        if node is None or node.value == value:
+            return node
+
+        if value < node.value:
+            return self._search_from_node(node.left, value)
+        else:
+            return self._search_from_node(node.right, value)
+
+    def delete(self, value: int) -> bool:
+        """
+        Удаление элемента из дерева
+        Сложность: в среднем O(log n), в худшем O(n)
+        """
+        node = self.search(value)
+        if node is None:
+            return False
+
+        self._delete_node(node)
+        self._size -= 1
+        return True
+
+    def _delete_node(self, node: TreeNode):
+        """Вспомогательный метод для удаления узла"""
+        if node.left is None and node.right is None:
+            self._transplant(node, None)
+        elif node.left is None:
+            self._transplant(node, node.right)
+        elif node.right is None:
+            self._transplant(node, node.left)
+        else:
+            successor = self.find_min(node.right)
+
+            if successor.parent != node:
+                self._transplant(successor, successor.right)
+                successor.right = node.right
+                successor.right.parent = successor
+
+            self._transplant(node, successor)
+            successor.left = node.left
+            successor.left.parent = successor
+
+    def _transplant(self, u: Optional[TreeNode], v: Optional[TreeNode]):
+        """Заменяет поддерево с корнем u на поддерево с корнем v"""
+        if u.parent is None:
+            self.root = v
+        elif u == u.parent.left:
+            u.parent.left = v
+        else:
+            u.parent.right = v
+
+        if v is not None:
+            v.parent = u.parent
+
+    def find_min(self, node: Optional[TreeNode] = None) -> Optional[TreeNode]:
+        """
+        Поиск минимального элемента в поддереве
+        Сложность: O(h), где h - высота поддерева
+        """
+        if node is None:
+            node = self.root
+            if node is None:
+                return None
+
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+
+    def find_max(self, node: Optional[TreeNode]) -> Optional[TreeNode]:
+        """
+        Поиск максимального элемента в поддереве
+        Сложность: O(h), где h - высота поддерева
+        """
+        if node is None:
+            node = self.root
+            if node is None:
+                return None
+
+        current = node
+        while current.right is not None:
+            current = current.right
+        return current
