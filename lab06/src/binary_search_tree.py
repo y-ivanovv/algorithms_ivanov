8b85fc016e2d98196473a373fb5e10127038cac7
y-ivanovv <yurastav126@mail.ru>
2025-11-26 12:44:39 +0300

    update bst

--- lab06/src/binary_search_tree.py
+++ lab06/src/binary_search_tree.py
@@ -10,10 +10,10 @@ class TreeNode:
         self.right = None
         self.parent = None
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Значение узла: {self.value}, Родитель узла: {self.parent}, Левый потомок: {self.left}, Правый потомок:{self.right}"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
 
@@ -148,3 +148,62 @@ class BinaryTree:
         while current.right is not None:
             current = current.right
         return current
+
+    def is_valid_bst(self) -> bool:
+        """
+        Проверка, является ли дерево корректным BST
+        Сложность: O(n)
+        """
+        return self._is_valid_node(self.root, -10**10, 10**10)
+
+    def _is_valid_node(self, node: Optional[TreeNode], min_val: int, max_val: int) -> bool:
+        """Рекурсивная проверка валидности BST"""
+        if node is None:
+            return True
+
+        if not (min_val < node.value < max_val):
+            return False
+
+        return (self._is_valid_node(node.left, min_val, node.value) and
+                self._is_valid_node(node.right, node.value, max_val))
+
+    def height(self, node: Optional[TreeNode] = None) -> int:
+        """
+        Вычисление высоты дерева/поддерева
+        Сложность: O(n)
+        """
+        if node is None:
+            node = self.root
+            if node is None:
+                return -1
+
+        return self._height_of_node(node)
+
+    def _height_of_node(self, node: Optional[TreeNode]) -> int:
+        """Рекурсивное вычисление высоты"""
+        if node is None:
+            return -1
+
+        left_height = self._height_of_node(node.left)
+        right_height = self._height_of_node(node.right)
+
+        return max(left_height, right_height) + 1
+
+    def size(self) -> int:
+        """Возвращает количество элементов в дереве"""
+        return self._size
+
+    def is_empty(self) -> bool:
+        """Проверяет, пусто ли дерево"""
+        return self.root is None
+
+    def clear(self):
+        """Очищает дерево"""
+        self.root = None
+        self._size = 0
+
+    def __len__(self) -> int:
+        return self._size
+
+    def __contains__(self, item: int) -> bool:
+        return self.search(item) is not None
